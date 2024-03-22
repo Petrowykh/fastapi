3a574826e9ca802997cad4f4d182ef34a1c0b905
Petrowykh <a.petrowykh@gmail.com>
2024-03-22 16:26:37 +0300

    v 0.11

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 from fastapi import FastAPI
+from tortoise.contrib.fastapi import register_tortoise
+
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get('/')
+def index():
+    return {'message': 'Hello'}
 
-@app.get("/posts")
-def get_posts():
-    return {"data": "This is your posts"}
-
-@app.post("/createposts")
-def create_posts():
-    return {"Message": "Success created post"}
+register_tortoise(
+    app,
+    db_url="sqlite://databse.sqlite3",
+    modules={"models" : ['models']},
+    generate_schemas=True,
+    add_exception_handlers=True
+)
